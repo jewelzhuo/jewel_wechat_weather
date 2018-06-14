8a4ef40218922cabf694f80de521e7fc076eaa61
zhuo <390152513@qq.com>
2018-06-14 11:51:55 +0800

    update wx:if

--- pages/index/index.js
+++ pages/index/index.js
@@ -23,10 +23,6 @@ const UNPROMPTED = 0;
 const UNAUTHORIZED = 1;
 const AUTHORIZED = 2;
 
-const UNPROMPTED_TIPS = "点击获取当前位置";
-const UNAUTHORIZED_TIPS = "点击开启位置权限";
-const AUTHORIZED_TIPS = "";
-
 Page({
   data: {
     nowTemp: '',
@@ -36,8 +32,7 @@ Page({
     todayDate: '',
     todayTemp: '',
     city: '上海市',
-    locationAuthType: UNPROMPTED,
-    locationTipsText: UNPROMPTED_TIPS
+    locationAuthType: UNPROMPTED
   },
   onPullDownRefresh(){
     this.getNow(function(){
@@ -45,29 +40,33 @@ Page({
     })
   },
   onLoad() {
-    this.qqmapsdk = new QQMapWX({
+    var that = this;
+    that.qqmapsdk = new QQMapWX({
       key: 'I77BZ-GZP3O-D77WO-S2F4Q-YAJXE-QZBQ6'
     });
     wx.getSetting({
       success: function(res) {
         let auth = res.authSetting['scope.userLocation'];
-        this.setData({
-          locationAuthType = auth ? AUTHORIZED : (auth === false) ? UNAUTHORIZED : UNPROMPTED,
-          locationTipsText = auth ? AUTHORIZED_TIPS : (auth === false) ? UNAUTHORIZED_TIPS : UNPROMPTED_TIPS,
-        })
+        let locationAuthType = auth ? AUTHORIZED : (auth === false) ? UNAUTHORIZED : UNPROMPTED;
+        that.setData({
+          locationAuthType: locationAuthType
+        });
         if(auth)
-          this.getCityAndWeather()
+          that.getCityAndWeather(that)
         else
-          this.getNow()
+          that.getNow()
       },
-    });
+      fail: function(){
+        that.getNow()
+      }
+    })
   },
   getNow(callback){
     var that = this;
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
       data: {
-        city: that.data.city,
+        city: that.data.city
       },
       success: function (res) {
         let result = res.data.result;
@@ -95,17 +94,17 @@ Page({
     });
   },
   setHourlyWeather(result, that){
-    let forecast = result.forecast
-    let hourlyWeather = []
-    let nowHour = new Date().getHours()
+    let forecast = result.forecast;
+    let hourlyWeather = [];
+    let nowHour = new Date().getHours();
     for (let i = 0; i < 8; i += 1) {
       hourlyWeather.push({
         time: (i * 3 + nowHour) % 24 + '时',
         iconPath: '/images/' + forecast[i].weather + '-icon.png',
         temp: forecast[i].temp + '°'
       })
-    }
-    hourlyWeather[0].time = '现在'
+    };
+    hourlyWeather[0].time = '现在';
     that.setData({
       hourlyWeather: hourlyWeather
     })
@@ -119,7 +118,7 @@ Page({
   },
   onTapDayWeather(){
     wx.navigateTo({
-      url: '/pages/list/list?city=' + this.data.city,
+      url: '/pages/list/list?city=' + this.data.city
     })
   },
   onTapLocation(){
@@ -129,19 +128,18 @@ Page({
         success: function(res){
           let auth = res.authSetting['scope.userLocation'];
           if(auth)
-            that.getCityAndWeather()
+            that.getCityAndWeather(that)
         }
       })
     else
-      that.getCityAndWeather()
+      that.getCityAndWeather(that)
   },
-  getCityAndWeather(){
+  getCityAndWeather(that){
     wx.getLocation({
       success: function(res){
         that.setData({
-          locationAuthType: AUTHORIZED,
-          locationTipsText: AUTHORIZED_TIPS
-        }),
+          locationAuthType: AUTHORIZED
+        });
         that.qqmapsdk.reverseGeocoder({
           location: {
             latitude: res.latitude,
@@ -159,10 +157,9 @@ Page({
       },
       fail: function(){
         that.setData({
-          locationAuthType: UNAUTHORIZED, 
-          locationTipsText: UNAUTHORIZED_TIPS
+          locationAuthType: UNAUTHORIZED
         })
       }
-    });
+    })
   }
 })
